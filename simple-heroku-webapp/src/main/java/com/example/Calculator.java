340a944ca5c20b6fd89c571c6f60ced5304e325e
Theodoros Chaikalis <theodoros.chaikalis@intrasoft-intl.com>
2020-10-28 13:17:12 +0200

    add calculator

---
+++ simple-heroku-webapp/src/main/java/com/example/Calculator.java
@@ -0,0 +1,30 @@
+package com.example;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ * Root resource (exposed at "calculator" path)
+ */
+@Path("calculator")
+public class Calculator {
+
+    /**
+     * Method handling HTTP GET requests. The returned object will be sent
+     * to the client as "text/plain" media type.
+     *
+     * @return String that will be returned as a text/plain response.
+     */
+    @GET
+    @Path("add/{a}/{b}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response adder(@PathParam("a") String a, @PathParam("b") String b) {
+        int result = Integer.valueOf(a) + Integer.valueOf(b);
+        return Response.status(200).entity(result).build();
+    }
+
+}
