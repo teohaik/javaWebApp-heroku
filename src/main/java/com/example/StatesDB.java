9b5bc0d1765f7b971e77b696760fe1c76d9302a7
teohaik <teohaik@gmail.com>
2020-11-13 19:31:09 +0200

    add us states rest

---
+++ src/main/java/com/example/StatesDB.java
@@ -0,0 +1,89 @@
+package com.example;
+
+import com.example.model.State;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StatesDB {
+
+    public static final List<State> states = new ArrayList<>();
+
+    static{
+        states.add(new State("AB","ALBERTA"));
+        states.add(new State("AK","ALASKA"));
+        states.add(new State("AL","ALABAMA"));
+        states.add(new State("AR","ARKANSAS"));
+        states.add(new State("AS","AMERICAN SAMOA"));
+        states.add(new State("AZ","ARIZONA"));
+        states.add(new State("BC","BRITISH COLUMBIA"));
+        states.add(new State("CA","CALIFORNIA"));
+        states.add(new State("CO","COLORADO"));
+        states.add(new State("CT","CONNECTICUT"));
+        states.add(new State("DC","DISTRICT OF COLUMBIA"));
+        states.add(new State("DE","DELAWARE"));
+        states.add(new State("FL","FLORIDA"));
+        states.add(new State("FM","MICRONESIA"));
+        states.add(new State("GA","GEORGIA"));
+        states.add(new State("GU","GUAM"));
+        states.add(new State("HI","HAWAII"));
+        states.add(new State("IA","IOWA"));
+        states.add(new State("ID","IDAHO"));
+        states.add(new State("IL","ILLINOIS"));
+        states.add(new State("IN","INDIANA"));
+        states.add(new State("KS","KANSAS"));
+        states.add(new State("KY","KENTUCKY"));
+        states.add(new State("LA","LOUISIANA"));
+        states.add(new State("MA","MASSACHUSETTS"));
+        states.add(new State("MB","MANITOBA"));
+        states.add(new State("MD","MARYLAND"));
+        states.add(new State("ME","MAINE"));
+        states.add(new State("MH","MARSHALL ISLANDS"));
+        states.add(new State("MI","MICHIGAN"));
+        states.add(new State("MN","MINNESOTA"));
+        states.add(new State("MO","MISSOURI"));
+        states.add(new State("MP","NORTHERN MARIANA ISLANDS"));
+        states.add(new State("MS","MISSISSIPPI"));
+        states.add(new State("MT","MONTANA"));
+        states.add(new State("NB","NEW BRUNSWICK"));
+        states.add(new State("NC","NORTH CAROLINA"));
+        states.add(new State("ND","NORTH DAKOTA"));
+        states.add(new State("NE","NEBRASKA"));
+        states.add(new State("NH","NEW HAMPSHIRE"));
+        states.add(new State("NJ","NEW JERSEY"));
+        states.add(new State("NL","NEWFOUNDLAND AND LABRADOR"));
+        states.add(new State("NM","NEW MEXICO"));
+        states.add(new State("NS","NOVA SCOTIA"));
+        states.add(new State("NT","NORTHWEST TERRITORIES"));
+        states.add(new State("NU","NUNAVUT"));
+        states.add(new State("NV","NEVADA"));
+        states.add(new State("NY","NEW YORK"));
+        states.add(new State("OH","OHIO"));
+        states.add(new State("OK","OKLAHOMA"));
+        states.add(new State("ON","ONTARIO"));
+        states.add(new State("OR","OREGON"));
+        states.add(new State("PA","PENNSYLVANIA"));
+        states.add(new State("PE","PRINCE EDWARD ISLAND"));
+        states.add(new State("PI","PACIFIC ISLANDS"));
+        states.add(new State("PR","PUERTO RICO"));
+        states.add(new State("PW","PALAU"));
+        states.add(new State("QC","QUEBEC"));
+        states.add(new State("RI","RHODE ISLAND"));
+        states.add(new State("SC","SOUTH CAROLINA"));
+        states.add(new State("SD","SOUTH DAKOTA"));
+        states.add(new State("SK","SASKATCHEWAN"));
+        states.add(new State("TN","TENNESSEE"));
+        states.add(new State("TX","TEXAS"));
+        states.add(new State("UM","U.S. MINOR OUTLYING ISLANDS"));
+        states.add(new State("UT","UTAH"));
+        states.add(new State("VA","VIRGINIA"));
+        states.add(new State("VI","VIRGIN ISLANDS"));
+        states.add(new State("VT","VERMONT"));
+        states.add(new State("WA","WASHINGTON"));
+        states.add(new State("WI","WISCONSIN"));
+        states.add(new State("WV","WEST VIRGINIA"));
+        states.add(new State("WY","WYOMING"));
+        states.add(new State("YT","YUKON TERRITORY"));
+    }
+
+}
---
+++ src/main/java/com/example/model/State.java
@@ -0,0 +1,14 @@
+package com.example.model;
+
+public class State {
+
+    String code;
+    String name;
+
+
+    public State(String code, String name) {
+        this.code = code;
+        this.name = name;
+    }
+
+}
